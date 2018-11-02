08aaefbd9171b9535fb814cf7b5828da19418593
Vilma Mäkitalo <vilma.makitalo@luukku.com>
2018-11-02 11:53:56 +0200

    kohta 3: using a database with Mongoose

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,10 @@ FROM node:8
 WORKDIR /usr/src/app
 
 COPY package*.json ./
+
 RUN npm install
+RUN npm install mongoose
+RUN npm install async
 
 COPY . .
 
--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://Vilmak:wwwmongotest100@ds249623.mlab.com:49623/local_library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
